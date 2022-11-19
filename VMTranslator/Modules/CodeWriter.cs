33fe4d9b26517666fe886e117457d8f1b39c4302
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-11-19 15:28:08 +0100

    Add project files.

---
+++ VMTranslator/Modules/CodeWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMTranslator.Modules
+{
+    public class CodeWriter
+    {
+        public string WriteArithmetic(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string WritePushPop(string commandType, string segement, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VMTranslator/Modules/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMTranslator.Modules
+{
+    public class Parser
+    {
+        private StreamReader sr;
+        public Parser(StreamReader sr)
+        {
+            this.sr = sr;
+        }
+
+        public string CommandType(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Arg1(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Arg2(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
