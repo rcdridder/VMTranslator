28123474b512e322f07facd19c656693eef659cc
Rob de Ridder <rob.deridder@itvitaelearning.nl>
2022-11-21 10:51:27 +0100

    Interfaces added.

---
+++ VMTranslator/Interfaces/ICodeWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMTranslator.Interfaces
+{
+    public interface ICodeWriter
+    {
+        /// <summary>
+        /// Returns arithmetic command in HACK-assembly.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        string WriteArithmetic(string command);
+        /// <summary>
+        /// Returns push/pop command in HACK-assembly.
+        /// </summary>
+        /// <param name="commandType"></param>
+        /// <param name="segement"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        string WritePushPop(string commandType, string segement, int index);
+    }
+}
---
+++ VMTranslator/Interfaces/IParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMTranslator.Interfaces
+{
+    public interface IParser
+    {
+        /// <summary>
+        /// Returns current command type.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        string CommandType(string command);
+        /// <summary>
+        /// Parses first argument of current command (if command = C_Arithmetic returns command itself).
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        string Arg1(string command);
+        /// <summary>
+        /// Parses second argument of current command (if command = C_Push/Pop/Function/Call).
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        int Arg2(string command);
+    }
+}
