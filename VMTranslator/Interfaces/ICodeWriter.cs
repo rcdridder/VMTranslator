3e7f50db9ea9b3e76e17293af4c8ec1627e6a3a1
Rob de Ridder <rderidder@outlook.com>
2023-03-30 14:09:00 +0200

    Updated CodeWriter interface.

--- VMTranslator/Interfaces/ICodeWriter.cs
+++ VMTranslator/Interfaces/ICodeWriter.cs
@@ -1,26 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VMTranslator.Interfaces
+﻿namespace VMTranslator.Interfaces
 {
     public interface ICodeWriter
     {
         /// <summary>
-        /// Returns arithmetic command in HACK-assembly.
+        /// Writes arithmetic command in HACK-assembly.
         /// </summary>
         /// <param name="command"></param>
         /// <returns></returns>
         void WriteArithmetic(string command);
         /// <summary>
-        /// Returns push/pop command in HACK-assembly.
+        /// Writes push/pop command in HACK-assembly.
         /// </summary>
         /// <param name="commandType"></param>
         /// <param name="segement"></param>
         /// <param name="index"></param>
         /// <returns></returns>
         void WritePushPop(string commandType, string segement, int index);
+        /// <summary>
+        /// Writes branching command in HACK-assembly.
+        /// </summary>
+        /// <param name="commandType"></param>
+        /// <param name="segment"></param>
+        /// <param name="index"></param>
+        void WriteBranching(string commandType, string dest);
+        /// <summary>
+        /// Writes function declaration in HACK-assembly.
+        /// </summary>
+        /// <param name="functioName"></param>
+        /// <param name="nVars"></param>
+        void WriteFunction(string functioName, int nVars);
+        /// <summary>
+        /// Writes call command in HACK-assembly. 
+        /// </summary>
+        /// <param name="functioName"></param>
+        /// <param name="nArgs"></param>
+        void WriteCall(string functioName, int nArgs);
+        /// <summary>
+        /// Writes return command in HACK-assembly.
+        /// </summary>
+        void WriteReturn();
     }
 }
